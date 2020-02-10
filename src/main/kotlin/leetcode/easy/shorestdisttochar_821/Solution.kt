a44cf11d5f7b0f5ec579b9ad320c2378ea4da654
kursivee <kursivee.dev@gmail.com>
2020-02-10 08:04:17 -0500

    [Leetcode][Algorithms][Easy][821] Shortest distance to a char nested loop solution

--- src/main/kotlin/leetcode/easy/shorestdisttochar_821/Solution.kt
+++ src/main/kotlin/leetcode/easy/shorestdisttochar_821/Solution.kt
@@ -19,10 +19,50 @@ class Solution {
         }
         return arr
     }
+
+    fun shortestToChar2(S: String, C: Char): IntArray {
+        val arr = IntArray(S.length)
+        var cIndex = -1
+        var currI = 0
+        S.forEachIndexed { index, c ->
+            if(c == C) {
+                for(i in currI until index) {
+                    arr[i] = if(cIndex == -1) {
+                        Math.min(S.length, index - i)
+                    } else {
+                        Math.min(i - cIndex, index - i)
+                    }
+                }
+                cIndex = index
+                currI = index
+            } else if(index == S.length - 1) {
+                for(i in currI..index) {
+                    arr[i] = if(cIndex == -1) {
+                        Math.min(S.length, index - i)
+                    } else {
+                        i - cIndex
+                    }
+                }
+            }
+        }
+        return arr
+    }
 }
 
 fun main() {
+    Solution().shortestToChar("aaea", 'e').also {
+        println(it.joinToString(", "))
+    }
+
     Solution().shortestToChar("loveleetcode", 'e').also {
+        println(it.joinToString(", "))
+    }
+
+    Solution().shortestToChar2("aaea", 'e').also {
+        println(it.joinToString(", "))
+    }
+
+    Solution().shortestToChar2("loveleetcode", 'e').also {
         println(it.joinToString(", "))
     }
 }
