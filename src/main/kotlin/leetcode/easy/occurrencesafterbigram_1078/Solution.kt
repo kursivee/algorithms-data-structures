cd15d49b5dfb5eea16ceb06af354e7e464f2be4d
kursivee <kursivee.dev@gmail.com>
2020-02-16 16:24:14 -0500

    [Leetcode][Algorithms][Easy][1078] Occurrences after bigram no regex solution

--- src/main/kotlin/leetcode/easy/occurrencesafterbigram_1078/Solution.kt
+++ src/main/kotlin/leetcode/easy/occurrencesafterbigram_1078/Solution.kt
@@ -3,26 +3,29 @@ package leetcode.easy.occurrencesafterbigram_1078
 // https://leetcode.com/problems/occurrences-after-bigram/
 class Solution {
     fun findOcurrences(text: String, first: String, second: String): Array<String> {
-        val arr = mutableListOf<String>()
-        Regex("(?=((\\b$first $second)\\s\\w+))").findAll(text).also {
-            it.forEach {
-                arr.add(it.groupValues[1].replace("$first $second ", "").trim())
+        val list = mutableListOf<String>()
+        val split = text.split(" ")
+        for(i in 0..split.size - 3) {
+            if(split[i] == first) {
+                if(split[i+1] == second) {
+                    list.add(split[i+2])
+                }
             }
         }
-        return arr.toTypedArray()
+        return list.toTypedArray()
     }
 }
 
 fun main() {
     Solution().findOcurrences("alice is a good girl she is a good student", "a", "good").also {
-        println(it.joinToString(", "))
+        println(it.joinToString(", ") == "girl, student")
     }
 
     Solution().findOcurrences("alice is aa good girl she is a good student", "a", "good").also {
-        println(it.joinToString(", "))
+        println(it.joinToString(", ") == "student")
     }
 
     Solution().findOcurrences("we will we will rock you", "we", "will").also {
-        println(it.joinToString(", "))
+        println(it.joinToString(", ") == "we, rock")
     }
 }
