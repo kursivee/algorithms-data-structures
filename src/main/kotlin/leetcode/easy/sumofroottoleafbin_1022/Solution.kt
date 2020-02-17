a85e1f8f89ca5d205a99f018a69ab55e80e9012c
kursivee <kursivee.dev@gmail.com>
2020-02-16 19:17:49 -0500

    [Leetcode][Algorithms][Easy][1022] sum of root to leaf binrary optimal solution

--- src/main/kotlin/leetcode/easy/sumofroottoleafbin_1022/Solution.kt
+++ src/main/kotlin/leetcode/easy/sumofroottoleafbin_1022/Solution.kt
@@ -14,44 +14,24 @@ import leetcode.easy.mergetwobinarytrees_617.TreeNode
  */
 // https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/submissions/
 class Solution {
-    private val ints: MutableList<List<Int>> = mutableListOf()
-
     fun sumRootToLeaf(root: TreeNode?): Int {
         if(root == null) return 0
-        traverse(root, mutableListOf())
-        return ints.fold(0) { acc, list ->
-            acc + list.binarySum()
-        }
+        return traverse(root, "")
     }
 
-    private fun traverse(root: TreeNode, list: MutableList<Int>) {
-        list.add(root.`val`)
+    private fun traverse(root: TreeNode, s: String): Int {
+        var sum = 0
+        val s1 = s + root.`val`
         if(root.left == null && root.right == null) {
-            ints.add(list)
+            sum += Integer.parseInt(s1, 2)
         }
-
         root.left?.let {
-            val copy = mutableListOf<Int>()
-            copy.addAll(list)
-            traverse(it, copy)
+            sum += traverse(it, s1)
         }
         root.right?.let {
-            val copy = mutableListOf<Int>()
-            copy.addAll(list)
-            traverse(it, copy)
+            sum += traverse(it, s1)
         }
-    }
-
-    private fun List<Int>.binarySum() : Int {
-        var n = (size - 1).toDouble()
-        return fold(0) { acc, i ->
-            var x = 0.0
-            if(i == 1) {
-                x = Math.pow(2.0, n)
-            }
-            n--
-            acc + x.toInt()
-        }
+        return sum
     }
 }
 
