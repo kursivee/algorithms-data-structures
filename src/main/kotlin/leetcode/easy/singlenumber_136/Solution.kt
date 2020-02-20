64962f8aa9b45b440d4dc13fd08e9db72b4209e4
kursivee <kursivee.dev@gmail.com>
2020-02-19 22:39:10 -0500

    [Leetcode][Algorithms][Easy][136] Single number XOR solution

--- src/main/kotlin/leetcode/easy/singlenumber_136/Solution.kt
+++ src/main/kotlin/leetcode/easy/singlenumber_136/Solution.kt
@@ -18,6 +18,11 @@ class Solution {
         }
         return target
     }
+
+    fun singleNumber2(nums: IntArray): Int =
+        nums.fold(0) { acc, i ->
+            acc xor i
+        }
 }
 
 fun main() {
@@ -34,6 +39,25 @@ fun main() {
     }
 
     Solution().singleNumber(intArrayOf(
+        4,1,2,1,2
+    )).also {
+        println(it)
+    }
+
+
+    Solution().singleNumber2(intArrayOf(
+        1,1,2,2,3
+    )).also {
+        println(it)
+    }
+
+    Solution().singleNumber2(intArrayOf(
+        2,2,1
+    )).also {
+        println(it)
+    }
+
+    Solution().singleNumber2(intArrayOf(
         4,1,2,1,2
     )).also {
         println(it)
