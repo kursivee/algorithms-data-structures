0844e3dc273b44e65dadd89c6dc5851fb8e9a704
kursivee <kursivee.dev@gmail.com>
2020-02-19 22:33:51 -0500

    [Leetcode][Algorithms][Easy][136] Single number solution

---
+++ src/main/kotlin/leetcode/easy/singlenumber_136/Solution.kt
@@ -0,0 +1,41 @@
+package leetcode.easy.singlenumber_136
+
+// https://leetcode.com/problems/single-number/
+class Solution {
+
+
+    fun singleNumber(nums: IntArray): Int {
+        nums.sort()
+        var count = 0
+        var target = nums[0]
+        nums.forEach {
+            if(target == it) count++
+            else {
+                if(count % 2 != 0) return target
+                target = it
+                count = 1
+            }
+        }
+        return target
+    }
+}
+
+fun main() {
+    Solution().singleNumber(intArrayOf(
+        1,1,2,2,3
+    )).also {
+        println(it)
+    }
+
+    Solution().singleNumber(intArrayOf(
+        2,2,1
+    )).also {
+        println(it)
+    }
+
+    Solution().singleNumber(intArrayOf(
+        4,1,2,1,2
+    )).also {
+        println(it)
+    }
+}
