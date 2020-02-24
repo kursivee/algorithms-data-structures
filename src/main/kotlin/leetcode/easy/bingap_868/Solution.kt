86f69ee4c2290e44495342e247c20858d85aafa9
kursivee <kursivee.dev@gmail.com>
2020-02-24 07:13:19 -0500

    [Leetcode][Algorithms][Easy][868] Binary gap without string conversion

--- src/main/kotlin/leetcode/easy/bingap_868/Solution.kt
+++ src/main/kotlin/leetcode/easy/bingap_868/Solution.kt
@@ -18,6 +18,26 @@ class Solution {
         }
         return max
     }
+
+    fun binaryGap2(N: Int): Int {
+        var x = N
+        var count = 0
+        var curr = false
+        var max = 0
+        while(x != 0) {
+            if(x % 2 == 1) {
+                if(curr) {
+                    count++
+                    max = Math.max(max, count)
+                    count = 0
+                } else {
+                    curr = true
+                }
+            } else if(curr) count++
+            x /= 2
+        }
+        return max
+    }
 }
 
 fun main() {
@@ -34,6 +54,24 @@ fun main() {
     }
 
     Solution().binaryGap(8).also {
+        println(it)
+    }
+
+    println()
+
+    Solution().binaryGap2(22).also {
+        println(it)
+    }
+
+    Solution().binaryGap2(5).also {
+        println(it)
+    }
+
+    Solution().binaryGap2(6).also {
+        println(it)
+    }
+
+    Solution().binaryGap2(8).also {
         println(it)
     }
 }
