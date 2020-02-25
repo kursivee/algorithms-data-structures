8b771706b175f0195fe593d0187b46e6e1d68212
kursivee <kursivee.dev@gmail.com>
2020-02-25 07:16:28 -0500

    [Leetcode][Algorithms][Easy][1331] Rank transform of array with bin search

--- src/main/kotlin/leetcode/easy/ranktransformofarray_1331/Solution.kt
+++ src/main/kotlin/leetcode/easy/ranktransformofarray_1331/Solution.kt
@@ -1,5 +1,7 @@
 package leetcode.easy.ranktransformofarray_1331
 
+import java.util.*
+
 // https://leetcode.com/problems/rank-transform-of-an-array/
 class Solution {
 
@@ -23,6 +25,33 @@ class Solution {
         }
         return arr
     }
+
+    // Timeout was happening because my previous solution with sorted set
+    // was iterating from 0..n to find the rank. Using binary search no longer causes
+    // timeout
+    fun arrayRankTransform2(arr: IntArray): IntArray {
+        val rank = arr.toSortedSet().toList()
+        for(i in arr.indices) {
+            arr[i] = binSearch(arr[i], rank)
+        }
+        return arr
+    }
+
+    private fun binSearch(target: Int, list: List<Int>, start: Int = 0, end: Int = list.size - 1): Int {
+        var s = start
+        var e = end
+        while(s <= e) {
+            val index = (s + e)/2
+            val i = list[index]
+            if(target == i) return index + 1
+            if(target > i) {
+                s = index + 1
+            } else {
+                e = index - 1
+            }
+        }
+        return -1
+    }
 }
 
 fun main() {
@@ -39,6 +68,25 @@ fun main() {
     }
 
     Solution().arrayRankTransform(intArrayOf(
+        37,12,28,9,100,56,80,5,12
+    )).also {
+        println(it.joinToString(", "))
+    }
+
+    println() 
+    Solution().arrayRankTransform2(intArrayOf(
+        40,10,20,30
+    )).also {
+        println(it.joinToString(", "))
+    }
+
+    Solution().arrayRankTransform2(intArrayOf(
+        100,100,100
+    )).also {
+        println(it.joinToString(", "))
+    }
+
+    Solution().arrayRankTransform2(intArrayOf(
         37,12,28,9,100,56,80,5,12
     )).also {
         println(it.joinToString(", "))
