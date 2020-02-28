76fb8a0becdc2eaaef187e30d0372a5f7643fa34
kursivee <kursivee.dev@gmail.com>
2020-02-28 07:02:40 -0500

    [Leetcode][Algorithms][Easy][349] Intersection of two arrays with bin search

--- src/main/kotlin/leetcode/easy/intersectiontwoarray_349/Solution.kt
+++ src/main/kotlin/leetcode/easy/intersectiontwoarray_349/Solution.kt
@@ -2,7 +2,7 @@ package leetcode.easy.intersectiontwoarray_349
 
 // https://leetcode.com/problems/intersection-of-two-arrays/
 class Solution {
-    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+    fun intersection2(nums1: IntArray, nums2: IntArray): IntArray {
         val small: IntArray
         val big: IntArray
         if(nums1.size > nums2.size) {
@@ -15,6 +15,38 @@ class Solution {
         return small.toSet().filter {
             big.contains(it)
         }.toIntArray()
+    }
+
+    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+        val small: IntArray
+        val big: IntArray
+        if(nums1.size > nums2.size) {
+            big = nums1
+            small = nums2
+        } else {
+            big = nums2
+            small = nums1
+        }
+        big.sort()
+        return small.toSet().filter {
+            big.binSearch(it)
+        }.toIntArray()
+
+    }
+
+    private fun IntArray.binSearch(target: Int): Boolean {
+        var l = 0
+        var r = size - 1
+        while(l <= r) {
+            val i = (l+r)/2
+            val n = get(i)
+            when {
+                n == target -> return true
+                n > target -> r = i - 1
+                n < target -> l = i + 1
+            }
+        }
+        return false
     }
 }
 
