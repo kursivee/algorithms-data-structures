18305ed04c271eb1b986216f4df6ba211f7ae23e
kursivee <kursivee.dev@gmail.com>
2020-02-28 07:12:15 -0500

    [Leetcode][Algorithms][Easy][1009] Complement of base 10 with bitshift

--- src/main/kotlin/leetcode/easy/complementbase10_1009/Solution.kt
+++ src/main/kotlin/leetcode/easy/complementbase10_1009/Solution.kt
@@ -2,11 +2,15 @@ package leetcode.easy.complementbase10_1009
 
 // https://leetcode.com/problems/complement-of-base-10-integer/
 class Solution {
-    fun bitwiseComplement(N: Int): Int {
+    fun bitwiseComplement2(N: Int): Int {
         val s = Integer.toBinaryString(N).map {
             if(it == '0') 1 else 0
         }.joinToString("")
         return Integer.parseInt(s, 2)
+    }
+
+    fun bitwiseComplement(N: Int): Int {
+        return N xor ((1.shl(N.toString(2).length) - 1))
     }
 }
 
