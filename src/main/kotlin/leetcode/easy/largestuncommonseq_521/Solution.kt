9d188def872f53521e292f156d17789c1302c6a8
kursivee <kursivee.dev@gmail.com>
2020-03-07 08:18:18 -0500

    [Leetcode][Algorithms][Easy][521] Longest uncommon subsequence i simpler solution

--- src/main/kotlin/leetcode/easy/largestuncommonseq_521/Solution.kt
+++ src/main/kotlin/leetcode/easy/largestuncommonseq_521/Solution.kt
@@ -1,14 +1,20 @@
 package leetcode.easy.largestuncommonseq_521
 
+import kotlin.math.max
+
 // https://leetcode.com/problems/longest-uncommon-subsequence-i/
 class Solution {
-    fun findLUSlength(a: String, b: String): Int {
+    fun findLUSlength2(a: String, b: String): Int {
         return when {
             a.length > b.length -> a.length
             a.length < b.length -> b.length
             a.contains(b) -> -1
             else -> a.length
         }
+    }
+
+    fun findLUSlength(a: String, b: String): Int {
+        return if(a == b) -1 else Math.max(a.length, b.length)
     }
 }
 
