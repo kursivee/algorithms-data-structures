3ae5d64e516288cab2088150b19a0cbd7a3e9f9f
kursivee <kursivee.dev@gmail.com>
2020-03-17 22:33:11 -0400

    [Leetcode][Algorithms][Easy][414] Third max number

---
+++ src/main/kotlin/leetcode/easy/thirdmaxnumber_414/Solution.kt
@@ -0,0 +1,57 @@
+package leetcode.easy.thirdmaxnumber_414
+
+import java.util.*
+
+// https://leetcode.com/problems/third-maximum-number/
+class Solution {
+    fun thirdMax(nums: IntArray): Int {
+        val map = mutableMapOf<Int, Int>()
+        var max0 = Integer.MIN_VALUE
+        var max1 = Integer.MIN_VALUE
+        var max2 = Integer.MIN_VALUE
+        nums.forEach {
+            map[it] = it
+            when {
+                it > max0 -> {
+                    max2 = max1
+                    max1 = max0
+                    max0 = it
+                }
+                it > max1 && it != max0 -> {
+                    max2 = max1
+                    max1 = it
+                }
+                it > max2 && it != max1 && it != max0 -> {
+                    max2 = it
+                }
+            }
+        }
+        return if(map.size < 3) max0 else max2
+    }
+}
+
+fun main() {
+    Solution().thirdMax(intArrayOf(
+        2,2,3,1
+    )).also {
+        println(it)
+    }
+
+    Solution().thirdMax(intArrayOf(
+        3,2,1
+    )).also {
+        println(it)
+    }
+
+    Solution().thirdMax(intArrayOf(
+        1,1,2
+    )).also {
+        println(it)
+    }
+
+    Solution().thirdMax(intArrayOf(
+        1,2,2,5,3,5
+    )).also {
+        println(it)
+    }
+}
