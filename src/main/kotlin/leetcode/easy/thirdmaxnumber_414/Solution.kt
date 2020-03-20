624be7c1387c4b8180dd2c987dbd06de3d933720
kursivee <kursivee.dev@gmail.com>
2020-03-19 22:48:12 -0400

    [Leetcode][Algorithms][Easy][414] Third maximum number maxHeap solution

--- src/main/kotlin/leetcode/easy/thirdmaxnumber_414/Solution.kt
+++ src/main/kotlin/leetcode/easy/thirdmaxnumber_414/Solution.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 // https://leetcode.com/problems/third-maximum-number/
 class Solution {
-    fun thirdMax(nums: IntArray): Int {
+    fun thirdMax2(nums: IntArray): Int {
         val map = mutableMapOf<Int, Int>()
         var max0 = Integer.MIN_VALUE
         var max1 = Integer.MIN_VALUE
@@ -26,7 +26,20 @@ class Solution {
                 }
             }
         }
-        return if(map.size < 3) max0 else max2
+        return if (map.size < 3) max0 else max2
+    }
+
+    fun thirdMax(nums: IntArray): Int {
+        val maxHeap = PriorityQueue(nums.size, compareByDescending<Int> { it })
+        nums.toSet().forEach {
+            maxHeap.add(it)
+        }
+        if(maxHeap.size < 3) {
+            return maxHeap.poll()
+        }
+        maxHeap.poll()
+        maxHeap.poll()
+        return maxHeap.poll()
     }
 }
 
@@ -37,17 +50,20 @@ fun main() {
         println(it)
     }
 
+    println()
     Solution().thirdMax(intArrayOf(
         3,2,1
     )).also {
         println(it)
     }
+    println()
 
     Solution().thirdMax(intArrayOf(
         1,1,2
     )).also {
         println(it)
     }
+    println()
 
     Solution().thirdMax(intArrayOf(
         1,2,2,5,3,5
