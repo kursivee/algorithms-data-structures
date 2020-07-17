20dd0757fb6fd5d604d9013d07d8086e32c45811
kursivee <kursivee.dev@gmail.com>
2020-07-16 21:29:57 -0400

    [Leetcode][Algorithms][Medium][1476] Subrectangle Queries

---
+++ src/main/kotlin/leetcode/medium/subrectanglequeries_1476/Solution.kt
@@ -0,0 +1,16 @@
+package leetcode.medium.subrectanglequeries_1476
+
+class SubrectangleQueries(private val rectangle: Array<IntArray>) {
+    fun updateSubrectangle(row1: Int, col1: Int, row2: Int, col2: Int, newValue: Int) {
+        for(i in row1..row2) {
+            for(j in col1..col2) {
+                rectangle[i][j] = newValue
+            }
+        }
+    }
+
+    fun getValue(row: Int, col: Int): Int {
+        return rectangle[row][col]
+    }
+
+}
