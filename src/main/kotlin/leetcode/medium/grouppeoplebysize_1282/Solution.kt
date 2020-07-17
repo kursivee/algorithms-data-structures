00e790d57be385c6649353dcfd26a25d7f7a808d
kursivee <kursivee.dev@gmail.com>
2020-07-16 22:20:51 -0400

    [Leetcode][Algorithms][Medium][1282] Group the people given the group size they belong to Improved

--- src/main/kotlin/leetcode/medium/grouppeoplebysize_1282/Solution.kt
+++ src/main/kotlin/leetcode/medium/grouppeoplebysize_1282/Solution.kt
@@ -4,19 +4,10 @@ class Solution {
     val map: MutableMap<Int, MutableList<Int>> = mutableMapOf()
 
     fun groupThePeople(groupSizes: IntArray): List<List<Int>> {
-        groupSizes.forEachIndexed { index, size ->
-            addToMap(index, size)
+        val grouped = groupSizes.withIndex().groupBy({ it.value }, { it.index })
+        return grouped.map { entry ->
+            entry.value.chunked(entry.key).flatten()
         }
-        val group: MutableList<List<Int>> = mutableListOf()
-        map.forEach { entry ->
-            group.addAll(entry.value.chunked(entry.key))
-        }
-        return group
-    }
-
-    private fun addToMap(index: Int, size: Int) {
-        val list = map.getOrPut(size, { mutableListOf() })
-        list.add(index)
     }
 }
 
