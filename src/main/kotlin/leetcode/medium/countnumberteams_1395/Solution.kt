13b3d675f32ccb69c30b4cf5da6ed0979012c279
kursivee <kursivee.dev@gmail.com>
2020-07-19 09:14:01 -0400

    [Leetcode][Algorithms][Medium][1395] Count number of teams n^2 solution with DP

--- src/main/kotlin/leetcode/medium/countnumberteams_1395/Solution.kt
+++ src/main/kotlin/leetcode/medium/countnumberteams_1395/Solution.kt
@@ -3,17 +3,54 @@ package leetcode.medium.countnumberteams_1395
 class Solution {
     fun numTeams(rating: IntArray): Int {
         var count = 0
-        for(i in 0 until rating.size - 2) {
-            for(j in i + 1 until rating.size - 1) {
-                for(k in i + 2 until rating.size) {
-                    val isIncreasing = (rating[i] < rating[j] && rating[j] < rating[k])
-                    val isDecreasing = (rating[i] > rating[j] && rating[j] > rating[k])
-                    if(
-                        (isIncreasing || isDecreasing) &&
-                        i < j &&  j < k
-                    ) {
-                        count++
-                    }
+
+        // dp array will keep track of how many elements are greater than it
+        var dp = IntArray(rating.size)
+        for(i in rating.indices) {
+            for(j in i-1 downTo 0) {
+                if(rating[i] < rating[j]) {
+                    // hey i have one element that's greater than me, increment my count.
+                    dp[i]++
+                    /**
+                     * ok here's the semi confusing part
+                     * so we know that there's at least a subsequence of length 2 here
+                     * since rating[i] < rating[j]
+                     * Now since the dp array is keeping track of how many elements are greater than
+                     * a given element, we can add the amount of elements greater than [j].
+                     * since these elements are greater than [j] than we can also assume that it is greater than [i].
+                     *
+                     * Take the following snapshot:
+                        assume we are now on rating[4]
+                        rating = [2, 5, 3, 4, 1]
+                        dp = [0, 0, 1, 1, 0]
+
+                        we do check to see if rating[4] < rating[3] or 1 < 4
+                        this is true so we can add the amount of elements less than 4 (tracked in dp[3])
+                        in this case there is only one element which we can see is rating[1] or 5
+                        and the subsequence we are now considering as a solution would be {1,4,5}
+
+                        let's say rating = [6,5,3,4,1]
+                        in this case dp[3] = 2, and the solution subsequences would be {1,4,5} and {1,4,6}
+                     * */
+                    count += dp[j]
+
+                }
+            }
+        }
+
+        /**
+         *  We also need to consider where rating[i] > rating[j] > rating[k]
+         * In order to do that we just need to do the same as previous except check if elements
+         * are less than it
+         */
+
+        // Resetting the dp count since we don't need to keep track of the greater than elements
+        dp = IntArray(rating.size)
+        for(i in rating.indices) {
+            for(j in i - 1 downTo 0) {
+                if(rating[i] > rating[j]) {
+                    dp[i]++
+                    count += dp[j]
                 }
             }
         }
