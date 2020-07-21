e5aa01f51f553e43a36a52535503e65634275cc6
kursivee <kursivee.dev@gmail.com>
2020-07-20 22:05:22 -0400

    [Leetcode][Algorithms][Medium][654] Maximum bin tree faster with copyOfRange

--- src/main/kotlin/leetcode/medium/maxbintree_654/Solution.kt
+++ src/main/kotlin/leetcode/medium/maxbintree_654/Solution.kt
@@ -5,14 +5,26 @@ import leetcode.easy.mergetwobinarytrees_617.TreeNode
 class Solution {
     fun constructMaximumBinaryTree(nums: IntArray): TreeNode? {
         if(nums.isEmpty()) return null
-        val max = nums.max()!!
+        val midIndex = nums.maxIndex()
+        val max = nums[midIndex]
         val node = TreeNode(max)
-        val midIndex = nums.indexOf(max)
-        val leftArr = nums.slice(0 until midIndex)
-        val rightArr = nums.slice(midIndex + 1 until nums.size)
-        node.left = constructMaximumBinaryTree(leftArr.toIntArray())
-        node.right = constructMaximumBinaryTree(rightArr.toIntArray())
+        val leftArr = nums.copyOfRange(0, midIndex)
+        val rightArr = nums.copyOfRange(midIndex + 1, nums.size)
+        node.left = constructMaximumBinaryTree(leftArr)
+        node.right = constructMaximumBinaryTree(rightArr)
         return node
+    }
+
+    fun IntArray.maxIndex(): Int {
+        var max = this[0]
+        var index = 0
+        for(i in 1 until size) {
+            if(this[i] > max) {
+                max = this[i]
+                index = i
+            }
+        }
+        return index
     }
 }
 
