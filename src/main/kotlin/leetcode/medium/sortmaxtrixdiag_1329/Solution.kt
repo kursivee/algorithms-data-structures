54752b6ff1c55309101b2a76be9463fdad37ea3f
kursivee <kursivee.dev@gmail.com>
2020-07-21 23:40:00 -0400

    [Leetcode][Algorithms][Medium][1329] Sort matrix diagonal fast with priority queue

--- src/main/kotlin/leetcode/medium/sortmaxtrixdiag_1329/Solution.kt
+++ src/main/kotlin/leetcode/medium/sortmaxtrixdiag_1329/Solution.kt
@@ -1,20 +1,21 @@
 package leetcode.medium.sortmaxtrixdiag_1329
 
+import java.util.*
+
 class Solution {
     fun diagonalSort(mat: Array<IntArray>): Array<IntArray> {
-        val map = mutableMapOf<Int, MutableList<Int>>()
+        val map = mutableMapOf<Int, PriorityQueue<Int>>()
         for(i in mat.indices) {
             for(j in mat[i].indices) {
-                val list = map.getOrDefault(i - j, mutableListOf())
-                list.add(mat[i][j])
-                map[i - j] = list
+                val queue = map.getOrDefault(i - j, PriorityQueue())
+                queue.add(mat[i][j])
+                map[i - j] = queue
             }
         }
 
-        map.values.forEach { it.sort() }
         for(i in mat.indices) {
             for(j in mat[i].indices) {
-                mat[i][j] = map[i - j]!!.removeAt(0)
+                mat[i][j] = map[i - j]!!.poll()
             }
         }
         return mat
