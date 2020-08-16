5eb8f97ed6ef8cc921ac21a797b1883beb538dde
kursivee <kursivee.dev@gmail.com>
2020-08-16 09:16:56 -0400

    [Leetcode][Algorithms][Easy][1351] Count negative numbers with bin search

--- src/main/kotlin/leetcode/easy/countnegnumsinsortedmatrix_1351/Solution.kt
+++ src/main/kotlin/leetcode/easy/countnegnumsinsortedmatrix_1351/Solution.kt
@@ -10,4 +10,27 @@ class Solution {
         }
         return count
     }
+
+    fun countNegativesBin(grid: Array<IntArray>): Int {
+        var count = 0
+        for(i in grid.indices) {
+            count += (grid[i].size - grid[i].indexOfNegative())
+        }
+        return count
+    }
+
+    private fun IntArray.indexOfNegative(): Int {
+        var l = 0
+        var r = this.size - 1
+        while(l < r) {
+            val mid = l + (r - l) / 2
+            if(this[mid] < 0) {
+                r = mid
+            } else {
+                l = mid + 1
+            }
+        }
+        if(this[l] >= 0) return this.size
+        return l
+    }
 }
