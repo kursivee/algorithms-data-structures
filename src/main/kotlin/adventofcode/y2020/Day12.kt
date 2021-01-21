802f1b1150d53dc4ea4d51eab08b972751a73ce7
kursivee <kursivee.dev@gmail.com>
2021-01-20 20:28:38 -0500

    [AOC2020][Day12] Rain risk

--- src/main/kotlin/adventofcode/y2020/Day12.kt
+++ src/main/kotlin/adventofcode/y2020/Day12.kt
@@ -25,11 +25,11 @@ class Day12 {
                             3 -> x -= n
                         }
                     } else {
-                        when((abs(deg + 360) % 360) / 90) {
-                            0 -> y += n
-                            1 -> x += n
-                            2 -> y -= n
-                            3 -> x -= n
+                        when((abs(deg) % 360) / 90) {
+                            1 -> y += n
+                            2 -> x += n
+                            3 -> y -= n
+                            0 -> x -= n
                         }
                     }
                 }
@@ -40,9 +40,62 @@ class Day12 {
                     deg += n
                 }
             }
-            println("$x $y")
         }
         return abs(x) + abs(y)
+    }
+
+    data class DataPoint(
+        val x: Int,
+        val y: Int
+    )
+
+    fun DataPoint.add(x: Int, y: Int): DataPoint {
+        return DataPoint(this.x + x, this.y + y)
+    }
+
+    fun solve2(list: List<String>): Int {
+        var ship = DataPoint(0, 0)
+        var waypoint = DataPoint(10, 1)
+        list.forEach { s ->
+            val command = s[0]
+            val n = s.substring(1 until s.length).toInt()
+            when(command) {
+                'N' -> waypoint = DataPoint(waypoint.x, waypoint.y + n)
+                'E' -> waypoint = DataPoint(waypoint.x + n, waypoint.y)
+                'S' -> waypoint = DataPoint(waypoint.x, waypoint.y - n)
+                'W' -> waypoint = DataPoint(waypoint.x - n, waypoint.y)
+                'R' -> {
+                    when((n % 360) / 90) {
+                        1 -> {
+                            waypoint = DataPoint(waypoint.y, waypoint.x * -1)
+                        }
+                        2 -> {
+                            waypoint = DataPoint(waypoint.x * -1, waypoint.y * -1)
+                        }
+                        3 -> {
+                            waypoint = DataPoint(waypoint.y * -1, waypoint.x)
+                        }
+                    }
+                }
+                'L' -> {
+                    when((n % 360) / 90) {
+                        3 -> {
+                            waypoint = DataPoint(waypoint.y, waypoint.x * -1)
+                        }
+                        2 -> {
+                            waypoint = DataPoint(waypoint.x * -1, waypoint.y * -1)
+                        }
+                        1 -> {
+                            waypoint = DataPoint(waypoint.y * -1, waypoint.x)
+                        }
+                    }
+                }
+                'F' -> {
+                    ship = ship.add(waypoint.x * n, waypoint.y * n)
+                }
+            }
+        }
+        return abs(ship.x) + abs(ship.y)
     }
 }
 
@@ -50,4 +103,5 @@ fun main() {
     val list = FileUtil.read("day12a.txt").split("\n")
     val solve = Day12()
     println(solve.solve(list))
+    println(solve.solve2(list))
 }
