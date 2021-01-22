734332e8880c2ceff60730edd7f3cf2ae9bbe92b
kursivee <kursivee.dev@gmail.com>
2021-01-22 15:53:00 -0500

    [Leetcode][Algorithms][Medium][161] Cleanup One edit distance

--- src/main/kotlin/leetcode/medium/oneeditdist_161/Solution.kt
+++ src/main/kotlin/leetcode/medium/oneeditdist_161/Solution.kt
@@ -1,59 +1,28 @@
 package leetcode.medium.oneeditdist_161
 
 class Solution {
-    // Yes I am very aware of what this looks like...
     fun isOneEditDistance(s: String, t: String): Boolean {
         if(s.isEmpty() || t.isEmpty()) {
             return Math.abs(s.length - t.length) == 1
         }
         if(Math.abs(s.length - t.length) > 1) return false
-        if(s.length == t.length) {
-            var diff = false
-            for(i in s.indices) {
-                if(s[i] != t[i]) {
-                    if(diff) return false
-                    else diff = true
-                }
-            }
-            return diff
-        }
-        if(s.length > t.length) {
-            var diff = false
-            var si = 0
-            var ti = 0
-            while(si < s.length) {
-                try {
-                    if(s[si] != t[ti]) {
-                        if(diff) return false
-                        diff = true
-                        ti--
-                    }
-                    si++
-                    ti++
-                } catch(e: Exception) {
-                    return diff xor (Math.abs(ti - si) == 0)
-                }
+        val longest = if(s.length >= t.length) s else t
+        val shortest = if(s.length < t.length) s else t
 
+        var diff = false
+        var li = 0
+        var si = 0
+        while(li < longest.length && si < shortest.length) {
+            if(longest[li] != shortest[si]) {
+                if(diff) return false
+                diff = true
+                if(longest.length != shortest.length) si--
             }
-            return diff
-        } else {
-            var diff = false
-            var si = 0
-            var ti = 0
-            while(ti < t.length) {
-                try {
-                    if(s[si] != t[ti]) {
-                        if(diff) return false
-                        diff = true
-                        si--
-                    }
-                    si++
-                    ti++
-                } catch(e: Exception) {
-                    return diff xor (Math.abs(ti - si) == 0)
-                }
-            }
-            return diff
+            li++
+            si++
         }
+        return if(li == longest.length) {
+            diff
+        } else diff xor (li == si)
     }
 }
